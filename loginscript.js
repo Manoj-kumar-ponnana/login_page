2cd57d90e6ca2515c58f7815463124843be99cc2
Manoj <manojkumarponnana@gmail.com>
2024-08-31 01:38:03 +0530

    inital commit

---
+++ loginscript.js
@@ -0,0 +1,27 @@
+function eyeclick(){
+    if(document.getElementById("eyeBlind").classList.value==="d-block"){
+        document.getElementById("eye").classList.add("d-block");
+        document.getElementById("eyeBlind").classList.add("d-none");
+        document.getElementById("eye").classList.remove("d-none");
+        document.getElementById("eyeBlind").classList.remove("d-block");
+        document.getElementById('pass').type='text'
+    }
+    else{
+        document.getElementById("eye").classList.add("d-none");
+        document.getElementById("eyeBlind").classList.add("d-block");
+        document.getElementById("eye").classList.remove("d-block");
+        document.getElementById("eyeBlind").classList.remove("d-none");
+        document.getElementById('pass').type='password'
+    }
+}
+function check(){
+    x=document.getElementById('user_id').value
+    y=document.getElementById('pass').value
+    res=document.getElementById('res')
+    if(x=='manoj'&&y=='2002'){
+        res.innerHTML='login successful'
+    }
+    else{
+        res.innerHTML='invalid credentials !!'
+    }
+}
